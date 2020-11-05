1ef882f9a75ea9064ffeeda25cd172d5aaf79f58
Amit Shabo <amitshabo123@gmail.com>
2020-11-05 23:05:21 +0200

    Game Database API + Design (js)

--- GameConsuleWebStore/wwwroot/js/site.js
+++ GameConsuleWebStore/wwwroot/js/site.js
@@ -5,6 +5,8 @@
 // Write your JavaScript code.
 var array = [];
 var games = [];
+var textH5 = document.createTextNode("Company Name: ");
+var textP = document.createTextNode("Games Owned: ");
 jQuery(document).ready(function () {
 
     const $valueSpan = $('.range-slider__value');
@@ -29,18 +31,68 @@ window.onload = function () {
 
     $.ajax(settings).done(function (response) {
         array = response.results;
-        console.log(response);
+        //console.log(response);
+       
         for (var i = 0; i < array.length; i++) {
             var game = new Object();
             game.img = array[i].image_background;
             game.name = array[i].name;
-            game.counter = array[i].games_count;
+            game.games_count = array[i].games_count;
             games.push(game);
         }
-        console.log(games);
- //       var src = document.getElementById("gamediv");
-   //     var img = document.createElement("img");
-   //     img.src = array[0].image_background;
-   //     src.appendChild(img);
+        draw(games);
+
     });
-};
+ 
+    function draw(games) {
+        var card = createCard();
+        editCard(card, games);
+        appendCard(card);
+
+    }
+    function createCard() {
+        const card = {
+            cardDiv: document.createElement("div"),
+            cardSecondDiv: document.createElement("div"),
+            cardImg: document.createElement("img"),
+            cardH5: document.createElement("h5"),
+            cardP: document.createElement("p"),
+          
+            
+        }
+    
+        return card;
+    }
+    function editCard(card, games) {
+        //console.log(games);
+        const { cardDiv, cardSecondDiv, cardImg, cardH5, cardP } = card;
+        var i = Math.floor(Math.random() * 11);
+        cardDiv.className = "card bg-dark text-white";
+        cardSecondDiv.className = "card-img-overlay";
+        cardImg.src = games[i].img;
+        cardImg.className = "card-img";
+        cardImg.style.height = "250px";
+        cardH5.innerText = games[i].name;
+        cardH5.className = "card-title";
+        //cardH5.style = "margin-left:440px";
+        //cardP.style = "margin-left:440px";
+        cardP.className = "card-text";
+        cardP.innerText = games[i].games_count;
+        card.style = "margin-top: 20px"
+      
+
+    }
+    function appendCard(card) {
+        const { cardDiv, cardSecondDiv, cardImg, cardH5, cardP } = card;
+        cardDiv.append(cardSecondDiv, cardImg);
+        cardSecondDiv.append(cardH5, cardP);
+        //textH5 = document.createTextNode("Company Name: ");
+        //TextP = document.createTextNode("Games Owned: ");
+        cardH5.parentNode.insertBefore(textH5, cardH5);
+        cardP.parentNode.insertBefore(textP, cardP);
+        var gamed = document.getElementById("gamediv");
+        gamed.append(cardDiv);
+    }
+    
+    
+}
