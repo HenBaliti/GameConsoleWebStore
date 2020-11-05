c5b85166f8bb54226a57172f2048b8598009d07b
Amit Shabo <amitshabo123@gmail.com>
2020-11-05 15:19:00 +0200

    api start design

--- GameConsuleWebStore/wwwroot/js/site.js
+++ GameConsuleWebStore/wwwroot/js/site.js
@@ -3,7 +3,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
+var array = [];
+var games = [];
 jQuery(document).ready(function () {
 
     const $valueSpan = $('.range-slider__value');
@@ -15,24 +16,31 @@ jQuery(document).ready(function () {
     });
 });
 window.onload = function () {
-	var settings = {
-		"async": true,
-		"crossDomain": true,
-		"url": "https://rawg-video-games-database.p.rapidapi.com/developers",
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
-			"x-rapidapi-key": "f6f05d56dcmsh4deccfd05227522p1c83efjsn04ee8a8c6cea"
-		}
-	}
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://rawg-video-games-database.p.rapidapi.com/developers",
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+            "x-rapidapi-key": "f6f05d56dcmsh4deccfd05227522p1c83efjsn04ee8a8c6cea"
+        }
+    }
 
-	$.ajax(settings).done(function (response) {
-		array = response.results;
-		console.log(response);
-		//console.log(array[0].name);
-		//var src = document.getElementById("gamediv");
-		//var img = document.createElement("img");
-		//img.src = array[0].image_background;
-		//src.appendChild(img);
-	});
+    $.ajax(settings).done(function (response) {
+        array = response.results;
+        console.log(response);
+        for (var i = 0; i < array.length; i++) {
+            var game = new Object();
+            game.img = array[i].image_background;
+            game.name = array[i].name;
+            game.counter = array[i].games_count;
+            games.push(game);
+        }
+        console.log(games);
+ //       var src = document.getElementById("gamediv");
+   //     var img = document.createElement("img");
+   //     img.src = array[0].image_background;
+   //     src.appendChild(img);
+    });
 };
