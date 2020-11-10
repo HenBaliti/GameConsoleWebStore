05dc2487d93d33bed9e5f0fd601f14f4f72d53bb
Omri Yehidi <yehidi@cs.colman.ac.il>
2020-11-11 00:45:55 +0200

    Added Required and Validation Fields to register page.

--- GameConsuleWebStore/Models/User.cs
+++ GameConsuleWebStore/Models/User.cs
@@ -10,11 +10,18 @@ namespace GameConsuleWebStore.Models
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name = "Full Name")]
+        [Required]
         public String Name { get; set; }
+
+        [Required]
         public String UserName { get; set; }
 
+        [StringLength(8, ErrorMessage = "{0} must be between {2}-{1} characters", MinimumLength = 6)]
+        [Required]
         public String Password { get; set; }
         public String UserType { get; set; }
+        //[Required]
         public String Email { get; set; }
         public ICollection<Order> Orders { get; set; }
 
